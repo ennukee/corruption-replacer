9986556d9cc2a10febf1db9bff3098be2f608ba2
Dylan Bowers <dylan.volibowers@gmail.com>
2020-05-20 14:17:47 -0400

    site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useReducer } from 'react';
+import processSimcInput from './util/processSimcInput';
+import { corruptionData } from './util/corruptionIds'
+import './App.scss';
 
 function App() {
+  const [ input, setInput ] = useState('# Paste your /simc input here');
+  const [ buttonFlair, setButtonFlair ] = useState(false)
+  const [ activeCorruptions, toggleCorruption ] = useReducer((state, id) => {
+    const newState = {...state};
+    newState[id] = !newState[id];
+    return newState;
+  }, {})
+  const submitInput = () => {
+    const output = processSimcInput(input, activeCorruptions)
+    setInput(output)
+    setButtonFlair(true)
+    setTimeout(() => setButtonFlair(false), 2500)
+  }
+  const handleCorruptionClick = (id) => {
+    toggleCorruption(id)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="inner-body">
+        <div id="title">Corruption Mass Cloner</div>
+        <div id="description">
+          <div className="main-body">
+            Paste your /simc input into the box below, then click on the corruptions
+            below that represent any corruption you want to get a copy of on your active gear.
+          </div>
+          <div className="main-body">
+            This will ignore all non-equipped gear to prevent absurdly large outputs. Equip the pieces you would ideally corrupt before inputting. 
+          </div>
+          <div id="credits">This may have bugs, it was made in about 2 hours. Hit up Chickenism on US-Icecrown ingame or enragednuke#0001 on Discord with any issues.</div>
+        </div>
+        <div id="input-area">
+          <textarea rows="8" onChange={(e) => setInput(e.target.value)} value={input}/>
+          <button
+            className={buttonFlair ? 'flair' : ''}
+            onClick={submitInput}
+          >
+            {buttonFlair ? 'Done! Copy the above into raidbots :)' : 'Clone'}
+          </button>
+        </div>
+        <div id="corruption-selector">
+          { corruptionData.map(({ name, id }) => (
+            <div
+              className={activeCorruptions[id] ? 'active' : 'inactive'}
+              onClick={() => handleCorruptionClick(id)}
+              key={name}
+            >
+              {name}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/util/processSimcInput.js
@@ -0,0 +1,44 @@
+import { corruptionIds, corruptionData } from './corruptionIds'
+
+export default (input, toggledCorruptions) => {
+    console.log(corruptionData)
+    const lines = input.split('\n')
+    const bagGearIndex = lines.findIndex(line => line.startsWith('###'))
+    const mainGear = lines.slice(0, bagGearIndex)
+    const corruptableSlots = [
+        'wrist',
+        'hands',
+        'waist',
+        'legs',
+        'feet',
+        'finger1',
+        'finger2',
+        'main_hand',
+        'off_hand',
+    ]
+    const corruptableSlotLines = []
+    mainGear.forEach(line => {
+        corruptableSlots.forEach(slot => {
+            if (line.startsWith(slot)) {
+                corruptableSlotLines.push(line)
+            }
+        })
+    })
+    const allExtraPieces = []
+    const cleansedLines = corruptableSlotLines.map(line => {
+        return corruptionIds.reduce((acc, id) => acc.replace(id, ''), line).replace('/,', ',').replace('//', '/')
+    })
+    allExtraPieces.push(...cleansedLines)
+
+    // { a: true, b: false, c: true } => [a, c]
+    const idsToAdd = Object.keys(toggledCorruptions).filter(id => toggledCorruptions[id])
+
+    // Now we begin to add each corruption we want
+    idsToAdd.forEach(id => {
+        // if (id.includes('/')) id = id.split('/')[0]
+        const corruptedLines = cleansedLines.map(line => line.replace('bonus_id=', `bonus_id=${id}/`))
+        allExtraPieces.push(...corruptedLines)
+    })
+
+    return `${mainGear.join('\n')}\n### GEAR FROM CLONER\n${allExtraPieces.map(line => `# ${line}`).join('\n')}`
+}
